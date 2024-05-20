5d50a537dd1e788c4d6c01dd57d95f5e55e78bd4
dragoslazea <ldragosbogdan@gmail.com>
2024-05-20 14:29:16 +0300

    changed example

--- src/examples/build_histo_detect_anomalies_example.py
+++ src/examples/build_histo_detect_anomalies_example.py
@@ -1,5 +1,6 @@
-import histo_building.builder as histogram_builder
-import histo_building.utils as build_utils
+import sys
+sys.path.append("..")
+from histo_building.utils import *
 import data_preprocessing.utils as data_utils
 import anomaly_detection.detector as anomaly_detector
 import anomaly_detection.utils as detect_utils
@@ -8,21 +9,29 @@ import concrete_aux.fhe_utils as fhe_utils
 dft = data_utils.read_data_from_csv()
 data_utils.rescale_data(dft)
 
-day1 = '7/1/2021'
-day2 = '7/2/2021'
+day1 = input("Provide a day for building the reference histogram (mm/dd/yyyy or m/d/yyyy): ")
+day2 = input("Provide a day for detecting anomalies (mm/dd/yyyy or m/d/yyyy): ")
 
 d1 = data_utils.get_data_by_day(dft, day1)
 d2 = data_utils.get_data_by_day(dft, day2)
 
 detect_config = detect_utils.get_configuration()
-(low_detect, high_detect) = build_utils.build_buckets(detect_config['reference_histogram']['bin_size'], detect_config['reference_histogram']['min'], detect_config['reference_histogram']['max'])
+(low_detect, high_detect) = build_buckets(detect_config['reference_histogram']['bin_size'], detect_config['reference_histogram']['min'], detect_config['reference_histogram']['max'])
 
-training_days_anomaly_detection = ['7/1/2021', '6/7/2021', '6/8/2021']
+nd = int(input("Number of training days for detecting anomalies: "))
+training_days_anomaly_detection = []
+for _ in range(nd):
+    training_days_anomaly_detection.append(input("Provide a day for training the anomaly detection circuit (mm/dd/yyyy or m/d/yyyy): "))
+
 training_data_anomaly_detection = []
 for day in training_days_anomaly_detection:
     training_data_anomaly_detection.append(data_utils.get_data_by_day(dft, day))
 
-training_days_histogram_building = ['6/11/2021', '6/12/2021', '6/13/2021']
+nb = int(input("Number of training days for building the reference histogram: "))
+training_days_histogram_building = []
+for _ in range(nb):
+    training_days_histogram_building.append(input("Provide a day for training the histogram building circuit (mm/dd/yyyy or m/d/yyyy): "))
+
 training_data_histogram_building = []
 for day in training_days_histogram_building:
     training_data_histogram_building.append(data_utils.get_data_by_day(dft, day))
